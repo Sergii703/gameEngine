71481b14fdffd173b799b7e39bf92fa620b05474
Sergii703 <sergey703@gmail.com>
2018-06-14 21:43:51 +0200

    Add project files.

---
+++ gameEngine/mission.h
@@ -0,0 +1,33 @@
+﻿#pragma once
+using namespace std;
+#include <string>
+///////////////////////////////////NUMER MISJI//////////////////////////////////
+
+int getCurrentMission(int x)					//numer misji się zmienia w zależności od koordynat gracza
+{
+	int mission = 0;
+	if ((x>0) && (x<400)) { mission = 0; }		//misję
+	if (x>400) { mission = 1; }					
+	if (x>700) { mission = 2; }					
+	if (x>2200) { mission = 3; }				
+
+	return mission;								//funkcja zwraca numer misji
+}
+
+
+
+/////////////////////////////////////TEKST MISJI/////////////////////////////////
+
+string getTextMission(int currentMission) {
+
+	string missionText = "";					//tekst misji i jego inicjalizacja
+
+	switch (currentMission)						//przjmuje numer misji
+	{
+	case 0: missionText = "\nBeginning \ninstruction"; break;
+	case 1: missionText = "\nMission 1\n\nThe first mission\n"; break;
+	case 2: missionText = "\nMission 2\n The second mission\n"; break;
+	}
+
+	return missionText;							//funkcja zwraca tekst
+};
---
+++ gameEngine/view.h
@@ -0,0 +1,14 @@
+﻿#pragma once
+#include <SFML/Graphics.hpp>
+using namespace sf;
+
+View view;											//tworzymy obiekt vie, ktory i jest kamerą
+
+void setPlayerCoordinateForView(float x, float y) { // funkcja dla sledzenie za graczem
+	float tempX = x; float tempY = y;
+													// przesunięcie kamery
+	if (y > 624) tempY = 624;						
+
+	view.setCenter(tempX, tempY);					//przekazujemy koordynata gracza
+
+}
